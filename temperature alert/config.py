7e87882eb522967391beb7b3d611eb522a39aff1
Ritesh8732 <47283395+Ritesh8732@users.noreply.github.com>
2021-02-24 15:15:25 +0530

    Update config.py

--- temperature alert/config.py
+++ temperature alert/config.py
@@ -3,4 +3,4 @@ Api_Key="6da73274-0cc6-4d3d-b358-422a4fe2d482" #Api key of the wifi module
 device_id="BOLT13169823" #device id of the wifi module
 Telegram_chat_id="@temperature_alert_sensor"
 Telegram_bot_id="bot101076702:1010767902:AAFWhz7e4XvTiAwDHly9nI0k8lTZieZAz64"
-Threshold="200"# threshold temperature above which alert will be given
+Threshold="200"    # threshold value above which alert will be given
